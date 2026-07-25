018f7f2d7b983729e07d04e59bbbf423f7e1fa74
Joachim Jablon <ewjoachim@gmail.com>
2026-07-25 12:59:54 +0200

    test: fix settings fixture after server_url -> site_url rename

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pathlib
 from typing import Any
 
 import pytest
+import yarl
 
 from daily_writing import i18n
 from daily_writing import settings as settings_module
@@ -14,7 +15,7 @@ def dw_settings(tmp_path: pathlib.Path, monkeypatch):
 
     def f(**kwargs: Any) -> settings_module.Settings:
         return settings_module.Settings(
-            server_url=pydantic.networks.HttpUrl("https://foo.bar"),
+            site_url=yarl.URL("https://foo.bar/"),
             # Make tests deterministic:
             site_name="Site Name",
             timezone="Europe/Paris",
