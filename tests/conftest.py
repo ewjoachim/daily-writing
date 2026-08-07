4860c93a4592875b124f917177137318dfde7784
Joachim Jablon <ewjoachim@gmail.com>
2026-08-08 01:26:59 +0200

    test: allow overriding dw_settings fixture defaults

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,14 +16,14 @@ def dw_settings(tmp_path: pathlib.Path, monkeypatch):
     (tmp_path / "README.md").write_text("# Test\n")
 
     def f(**kwargs: Any) -> settings_module.Settings:
-        return settings_module.Settings(
-            site_url=yarl.URL("https://foo.bar/"),
+        defaults: dict[str, Any] = {
+            "site_url": yarl.URL("https://foo.bar/"),
             # Make tests deterministic:
-            site_name="Site Name",
-            timezone="Europe/Paris",
-            locale=i18n.Locale.from_string("fr-fr"),
-            **kwargs,
-        )
+            "site_name": "Site Name",
+            "timezone": "Europe/Paris",
+            "locale": i18n.Locale.from_string("fr-fr"),
+        }
+        return settings_module.Settings(**{**defaults, **kwargs})
 
     return f
 
